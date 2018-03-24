6d01dc97e4772868b5dcd3aa5c3fe889897e6379
kenleegit <ahkenlee@yahoo.com.hk>
2018-03-24 17:06:21 +0800

    Define Static Resources here for XAML views

---
+++ NewsRead/Views/ItemsVPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NewsRead
+{
+    public partial class ItemsVPage : ContentPage
+    {
+        public ItemsVPage(int InputParam)
+        {
+            InitializeComponent();
+
+            labelz.Text = "ABC123";
+        }
+    }
+}
---
+++ NewsRead/Views/ModalPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NewsRead.Views
+{
+    public partial class ModalPage : ContentPage
+    {
+        public ModalPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
